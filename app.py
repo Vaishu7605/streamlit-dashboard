b67e66f72b1729da6088942322e0c63c1382b560
Vaishu7605 <122589971+Vaishu7605@users.noreply.github.com>
2023-12-08 10:11:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if f1 is not None:
     st.write(filename)
     df= pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
-    os.chdir(r"C:\Users\vaish\Desktop\streamlit")
+    os.chdir(r"C:\Users\vaish\Desktop\Project\streamlit")
     df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
 
 col1, col2 = st.columns((2))
